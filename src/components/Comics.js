1d9cb62a926cc572ffbaac94635b15ee086e5fb0
Mohammed Nasser <mohammed_nasser.94@outlook.com>
2021-06-18 23:49:01 +0200

    fixing issue

--- src/components/Comics.js
+++ src/components/Comics.js
@@ -17,8 +17,8 @@ function Comics(props) {
   return (
     <Container>
       <div className="d-flex justify-content-center" style={{'marginBottom' : 40}}>
-        <Link to="/comics/english" className="remove-decoration" style={{marginRight: 20}}><h1 className={`hand-text ${language !== 'english' ? 'inactive' : ''}`}>English Comics</h1></Link>
-        <Link to="/comics/arabic" className="remove-decoration" style={{marginLeft: 20}}><h1 className={`hand-text ${language !== 'arabic' ? 'inactive' : ''}`}>Arabic Comics</h1></Link>
+        <Link to="/comics/english" className="remove-decoration" style={{marginRight: 20}}><h1 className={`hand-text ${language !== 'english' ? 'inactive-comics' : ''}`}>English Comics</h1></Link>
+        <Link to="/comics/arabic" className="remove-decoration" style={{marginLeft: 20}}><h1 className={`hand-text ${language !== 'arabic' ? 'inactive-comics' : ''}`}>Arabic Comics</h1></Link>
       </div>
       <div style={{display: language !== 'english' ? 'none' : 'block'}}>
         <PhotoGallery tag='english_comics'/>
