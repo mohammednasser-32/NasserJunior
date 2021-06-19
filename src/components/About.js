0705578065b59dd36b4649297f188b7a0aec0376
Mohammed Nasser <mohammed_nasser.94@outlook.com>
2021-06-19 13:03:52 +0200

    spelling mistake

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,7 @@ function About() {
           <p className="hand-text about-text">
             {"Hello there, my name is Mohammed Nasser, AKA Nasser Junior, an illustrator and a cartoonist \
 based in Alexandria, Egypt. I am also a software engineer (I did this website using Reactjs lol).\n\
-I started making comics to express my thoughts, feelings and daily struggles. wether it was college, work or anxiety.. \
+I started making comics to express my thoughts, feelings and daily struggles. whether it was college, work or anxiety.. \
 every situation had a comic for it. My comics went viral on different social media platforms gaining over 200,000 followers on Facebook!\n\
 Taking the next step in this path, I started to work commercially to help other brands and projects express their message and identity the same way I express \
 mine.\nIt brings joy to my heart being able to put someone`s thoughts into a sketch..so, I am looking forward for our \
