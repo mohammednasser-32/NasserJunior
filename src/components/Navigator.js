2984ff04e90551d4f90ff1f13b5cb66365e233a4
Mohammed Nasser <mohammed_nasser.94@outlook.com>
2021-06-21 17:29:30 +0200

    enahncing hover strokes

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -1,50 +1,21 @@
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 import { Container, Row, Col, Image } from 'react-bootstrap';
 import pencil from './../assets/misc/pencil.png'
-import pencilDashed from './../assets/misc/pencil-dashed.png'
+import pencilDashed from './../assets/misc/pencil-dashed.gif'
 import comics from './../assets/misc/comics.png'
-import comicsDashed from './../assets/misc/comics-dashed.png'
+import comicsDashed from './../assets/misc/comics-dashed.gif'
 import work from './../assets/misc/work.png'
-import workDashed from './../assets/misc/work-dashed.png'
+import workDashed from './../assets/misc/work-dashed.gif'
 import about from './../assets/misc/about.png'
-import aboutDashed from './../assets/misc/about-dashed.png'
+import aboutDashed from './../assets/misc/about-dashed.gif'
 
 function Navigator() {
   function addDashes(name) {
-    let image = document.getElementById(`${name}-image`)
-    switch(name) {
-      case 'pencil':
-        image.src = pencilDashed
-        break;
-      case 'comics':
-        image.src = comicsDashed
-        break;
-      case 'projects':
-        image.src = workDashed
-        break;
-      case 'about':
-        image.src = aboutDashed
-        break;
-  
-    } 
+    document.getElementById(`${name}-image-dashed`).style.display = 'block'
   }
 
   function removeDashes(name) {
-    let image = document.getElementById(`${name}-image`)
-    switch(name) {
-      case 'pencil':
-        image.src = pencil
-        break;
-      case 'comics':
-        image.src = comics
-        break;
-      case 'projects':
-        image.src = work
-        break;
-      case 'about':
-        image.src = about
-        break;
-    } 
+    document.getElementById(`${name}-image-dashed`).style.display = 'none'
   }
 
   return (
@@ -55,6 +26,7 @@ function Navigator() {
             <div>
               <Link to="/illustrations" className="remove-decoration d-flex flex-column" onMouseEnter={(e) => {addDashes('pencil')}} onMouseLeave={(e) => {removeDashes('pencil')}}>
                 <Image src={pencil} className="client-image p-5" id="pencil-image"/>
+                <Image src={pencilDashed} style={{'display' : 'none','position': 'absolute', 'zIndex': -1}}className="client-image p-5" id="pencil-image-dashed"/>
                 <h2 className="text-center hand-text">Illustrations</h2>
               </Link>
             </div>
@@ -62,18 +34,21 @@ function Navigator() {
           <Col xs={12} sm={6} md={3} className="d-flex justify-content-center p-4">
             <Link to="/comics/english" className="remove-decoration d-flex flex-column" onMouseEnter={(e) => {addDashes('comics')}} onMouseLeave={(e) => {removeDashes('comics')}}>
               <Image src={comics} className="client-image p-5" id="comics-image"/>
+              <Image src={comicsDashed} style={{'display' : 'none','position': 'absolute', 'zIndex': -1}}className="client-image p-5" id="comics-image-dashed"/>
               <h2 className="text-center hand-text">Comics</h2>
             </Link>
           </Col>
           <Col xs={12} sm={6} md={3} className="d-flex justify-content-center p-4">
             <Link to="/work" className="remove-decoration d-flex flex-column" onMouseEnter={(e) => {addDashes('projects')}} onMouseLeave={(e) => {removeDashes('projects')}}>
               <Image src={work} className="client-image p-5" id="projects-image"/>
+              <Image src={workDashed} style={{'display' : 'none','position': 'absolute', 'zIndex': -1}}className="client-image p-5" id="projects-image-dashed"/>
               <h2 className="text-center hand-text">Projects</h2>
             </Link>
           </Col>
           <Col xs={12} sm={6} md={3} className="d-flex justify-content-center p-4">
             <Link to="/about" className="remove-decoration d-flex flex-column" onMouseEnter={(e) => {addDashes('about')}} onMouseLeave={(e) => {removeDashes('about')}}>
               <Image src={about} className="client-image p-5" id="about-image"/>
+              <Image src={aboutDashed} style={{'display' : 'none','position': 'absolute', 'zIndex': -1}}className="client-image p-5" id="about-image-dashed"/>
               <h2 className="text-center hand-text">About</h2>
             </Link>
           </Col>
