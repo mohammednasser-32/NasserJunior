eba59ebc66c7e837c38bb9c4c11099059d0f6e9e
mohammednasser-jt <mohammed.nasser.ext@jobandtalent.com>
2023-06-03 11:24:53 +0300

    add whatsapp stickers and shop

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -6,13 +6,15 @@ import arabicComics from './../assets/misc/arabic-comics.png'
 import comicsSeries from './../assets/misc/comics-series.png'
 import work from './../assets/misc/work.png'
 import about from './../assets/misc/about.png'
+import whatsapp from './../assets/misc/whatsapp.png'
+import shop from './../assets/misc/shop.png'
 
 function Navigator() {
 
   return (
     <div className="p-4 grey-bg">
         <Row>
-          <Col xs={12} sm={6} md={4} className="d-flex justify-content-center p-4">
+          <Col xs={12} sm={6} md={3} className="d-flex justify-content-center p-4">
             <Link to="/illustrations" className="remove-decoration d-flex flex-column" >
               <div className="navigator-image-container">
                 <Image src={pencil} className="navigator-image"/>
@@ -20,7 +22,7 @@ function Navigator() {
               <h2 className="text-center hand-text">Illustrations</h2>
             </Link>
           </Col>
-          <Col xs={12} sm={6} md={4} className="d-flex justify-content-center p-4">
+          <Col xs={12} sm={6} md={3} className="d-flex justify-content-center p-4">
             <Link to="/comics/english" className="remove-decoration d-flex flex-column" >
               <div className="navigator-image-container">
                 <Image src={englishComics} className="navigator-image"/>
@@ -28,7 +30,7 @@ function Navigator() {
               <h2 className="text-center hand-text">English Comics</h2>
             </Link>
           </Col>
-          <Col xs={12} sm={6} md={4} className="d-flex justify-content-center p-4">
+          <Col xs={12} sm={6} md={3} className="d-flex justify-content-center p-4">
             <Link to="/comics/arabic" className="remove-decoration d-flex flex-column" >
               <div className="navigator-image-container">
                 <Image src={arabicComics} className="navigator-image"/>
@@ -36,7 +38,7 @@ function Navigator() {
               <h2 className="text-center hand-text">Arabic Comics</h2>
             </Link>
           </Col>
-          <Col xs={12} sm={6} md={4} className="d-flex justify-content-center p-4">
+          <Col xs={12} sm={6} md={3} className="d-flex justify-content-center p-4">
             <Link to="/comics-series" className="remove-decoration d-flex flex-column" >
               <div className="navigator-image-container">
                 <Image src={comicsSeries} className="navigator-image"/>
@@ -44,7 +46,23 @@ function Navigator() {
               <h2 className="text-center hand-text">Comics Series</h2>
             </Link>
           </Col>
-          <Col xs={12} sm={6} md={4} className="d-flex justify-content-center p-4">
+          <Col xs={12} sm={6} md={3} className="d-flex justify-content-center p-4">
+            <a href="https://getstickerpack.com/publisher/mohammed-nasser" target="_blank" className="remove-decoration d-flex flex-column">
+                <div className="navigator-image-container">
+                  <Image src={whatsapp} className="navigator-image"/>
+                </div>
+                <h2 className="text-center hand-text">WhatsApp Stickers</h2>
+            </a>
+          </Col>
+          <Col xs={12} sm={6} md={3} className="d-flex justify-content-center p-4">
+            <a href="https://microbusstore.com/collections/microbus-nasser" target="_blank" className="remove-decoration d-flex flex-column">
+              <div className="navigator-image-container">
+                <Image src={shop} className="navigator-image"/>
+              </div>
+              <h2 className="text-center hand-text">Shop</h2>
+            </a>
+          </Col>
+          <Col xs={12} sm={6} md={3} className="d-flex justify-content-center p-4">
             <Link to="/work" className="remove-decoration d-flex flex-column">
               <div className="navigator-image-container">
                 <Image src={work} className="navigator-image"/>
@@ -52,7 +70,7 @@ function Navigator() {
               <h2 className="text-center hand-text">Projects</h2>
             </Link>
           </Col>
-          <Col xs={12} sm={6} md={4} className="d-flex justify-content-center p-4">
+          <Col xs={12} sm={6} md={3} className="d-flex justify-content-center p-4">
             <Link to="/about" className="remove-decoration d-flex flex-column">
               <div className="navigator-image-container">
                 <Image src={about} className="navigator-image"/>
