84dfa258c2ba5a7b76ba5b71c1bf4329a33e12cc
mohammednasser-jt <mohammed.nasser.ext@jobandtalent.com>
2023-06-03 12:09:09 +0300

    fix build

--- src/components/ComicsSeries.js
+++ src/components/ComicsSeries.js
@@ -3,6 +3,7 @@ import { Row, Col, Image } from 'react-bootstrap';
 import company from './../assets/misc/company.png';
 import wonderer from './../assets/misc/wanderer.png';
 import programmerDiaries from './../assets/misc/programmer-diaries.png';
+import dispersedHeritage from './../assets/misc/egypts-dispersed-heritage.png';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 function ComicsSeries() {
@@ -11,19 +12,24 @@ function ComicsSeries() {
       <div className="series-container">
         <h1 className="hand-text text-center" style={{'marginBottom' : 40}}>Comics Series</h1>
         <Row>
-          <Col xs={12} sm={6} md={4} className="d-flex justify-content-center p-4">
+          <Col xs={12} sm={6} md={4} className="d-flex justify-content-center p-4 series-image-container">
             <Link to="/comics-series/wonderer" className="remove-decoration d-flex flex-column" >
               <Image src={wonderer} className="series-image"/>
             </Link>
           </Col>
-          <Col xs={12} sm={6} md={4} className="d-flex justify-content-center p-4">
+          <Col xs={12} sm={6} md={4} className="d-flex justify-content-center p-4 series-image-container">
             <Link to="/comics-series/programmer-diaries" className="remove-decoration d-flex flex-column" >
               <Image src={programmerDiaries} className="series-image"/>
             </Link>
           </Col>
-          <Col xs={12} sm={6} md={4} className="d-flex justify-content-center p-4">
+          <Col xs={12} sm={6} md={4} className="d-flex justify-content-center p-4 series-image-container">
             <Link to="/comics-series/company" className="remove-decoration d-flex flex-column" >
               <Image src={company} className="series-image"/>
+            </Link>
+          </Col>
+          <Col xs={12} sm={6} md={4} className="d-flex justify-content-center p-4 series-image-container">
+            <Link to="/comics-series/egypts-dispersed-heritage" className="remove-decoration d-flex flex-column" >
+              <Image src={dispersedHeritage} className="series-image"/>
             </Link>
           </Col>
         </Row>
--- src/components/Header.js
+++ src/components/Header.js
@@ -13,6 +13,8 @@ export default function Header(props) {
         <Link to="/comics/english" className='remove-decoration p-3'><h4 className={"hand-text header-text " + (path.includes('english') ? 'active-link' : 'inactive-link')}>English Comics</h4></Link>
         <Link to="/comics/arabic" className='remove-decoration p-3'><h4 className={"hand-text header-text " + (path.includes('arabic') ? 'active-link' : 'inactive-link')}>Arabic Comics</h4></Link>
         <Link to="/comics-series" className='remove-decoration p-3'><h4 className={"hand-text header-text " + (path.includes('series') ? 'active-link' : 'inactive-link')}>Comics Series</h4></Link>
+        <a href="https://getstickerpack.com/publisher/mohammed-nasser" target="_blank" className='remove-decoration p-3'><h4 className={"hand-text header-text inactive-link"}>WhatsApp Stickers</h4></a>
+        <a href="https://microbusstore.com/collections/microbus-nasser" target="_blank" className='remove-decoration p-3'><h4 className={"hand-text header-text inactive-link"}>Shop</h4></a>
         <Link to="/work" className='remove-decoration p-3 '><h4 className={"hand-text header-text " + (path.includes('work') ? 'active-link' : 'inactive-link')}>Projects</h4></Link>
         <Link to="/about" className='remove-decoration p-3'><h4 className={"hand-text header-text " + (path.includes('about') ? 'active-link' : 'inactive-link')}>About</h4></Link>
       </div>
